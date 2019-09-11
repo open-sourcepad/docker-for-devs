f9d5202a654c7f97c588846322a613b493db3d27
Mikee <mikeed@sourcepad.com>
2019-09-11 15:05:26 +0800

    NG changes

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -10,12 +10,17 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   api_version;
   definition;
+  remoteApi = 'http://0.0.0.0:3050/api';
+  doneScan = false;
+  remoteApiList = [];
+
   constructor(private http: HttpClient) {
     this.getFromApi();
+    this.scan();
   }
 
   getFromApi() {
-    this.http.get('http://0.0.0.0:3050/api/health_check').subscribe(e => {
+    this.http.get(`${this.remoteApi}/health_check`).subscribe(e => {
       this.api_version = e["api_version"];
     });
   }
@@ -24,8 +29,33 @@ export class AppComponent {
     this.definition = '';
     const value = document.getElementById('word')['value'];
 
-    this.http.get(`http://0.0.0.0:3050/api/define?word=${value}`).subscribe(e => {
+    this.http.get(`${this.remoteApi}/define?word=${value}`).subscribe(e => {
       this.definition = e["definition"];
     });
+  }
+
+  scan() {
+    const prefix = 'http://192.168.1.';
+    let ip = 100;
+
+    while(ip < 256) {
+      this.http.get(`${prefix + ip}/health_check`).subscribe(e => {
+        if(e) {
+          if(e["api_version"]) {
+            this.remoteApiList.push( {
+              ip: `${prefix + ip}`,
+              data: e
+            })
+          }
+        }
+      }, error => {
+
+      });
+
+      ip++;
+    }
+
+    this.doneScan = true;
+
   }
 }
